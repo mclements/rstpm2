6cb1b7b67d49a2157cda821622cd15b0ef704735
Mark Clements <mark.clements@ki.se>
2018-06-15 10:40:32 +0200

    Revert adding the names (only sensible for full data predictions)

--- R/pm2-3.R
+++ R/pm2-3.R
@@ -1484,7 +1484,6 @@ predict.stpm2.base <-
     }
     if (!se.fit) {
         out <- local(object,newdata,type=type,exposed=exposed,  ...)
-        names(out) <- object@args$X
     } else {
       gd <- NULL
       beta <- coef(object)
@@ -1594,7 +1593,6 @@ predict.stpm2.base <-
       if (link=="cloglog") 
           out <- data.frame(Estimate=out$Estimate,lower=out$upper,upper=out$lower)
       out <- invlinkf(out)
-      rownames(out) <- object@args$X
     }
     if (keep.attributes)
         attr(out,"newdata") <- newdata
