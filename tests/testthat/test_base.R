762bc5640f4e00dae1d69a074bb52e3264f15ad6
Mark Clements <mark.clements@ki.se>
2019-11-26 09:51:43 +0100

    Add test for interval-censored values

--- tests/testthat/test_base.R
+++ tests/testthat/test_base.R
@@ -41,6 +41,61 @@ test_that("probit", {
     expect_eps(coef(fit)[2], -0.282318, 1e-5)
 })
 
+
+test_that("interval", {
+    ## ICPHREG example
+    read.textConnection <- function(text, ...) {
+        conn <-  textConnection(text)
+        on.exit(close(conn))
+        read.table(conn, ...)
+    }
+    hiv <- read.textConnection("0 16 0 0 0 1
+15 26 0 0 0 1
+12 26 0 0 0 1
+17 26 0 0 0 1
+13 26 0 0 0 1
+0 24 0 0 1 0
+6 26 0 1 1 0
+0 15 0 1 1 0
+14 26 0 1 1 0
+12 26 0 1 1 0
+13 26 0 1 0 1
+12 26 0 1 1 0
+12 26 0 1 1 0
+0 18 0 1 0 1
+0 14 0 1 0 1
+0 17 0 1 1 0
+0 15 0 1 1 0
+3 26 1 0 0 1
+4 26 1 0 0 1
+1 11 1 0 0 1
+13 19 1 0 0 1
+0 6 1 0 0 1
+0 11 1 1 0 0
+6 26 1 1 0 0
+0 6 1 1 0 0
+2 12 1 1 0 0
+1 17 1 1 1 0
+0 14 1 1 0 0
+0 25 1 1 0 1
+2 11 1 1 0 0
+0 14 1 1 0 0")
+    names(hiv) <- c("Left","Right","Stage","Dose","CdLow","CdHigh")
+    hiv <- transform(hiv,Event = ifelse(Left==0,2,
+                                 ifelse(Right>=26,0,
+                                        3)))
+    hiv2 <- transform(hiv,
+                      Left = ifelse(Event==2,Right,
+                             ifelse(Event==0,Left,
+                                    Left)),
+                      Right = ifelse(Event==2,Inf,
+                              ifelse(Event==0,Inf,
+                                     Right)))
+    fit <- stpm2(Surv(Left,Right,Event,type="interval")~Stage, data=hiv2, df=2)
+    expect_eps(coef(fit)[2], 1.917699, 1e-4)
+})
+
+
 context("stpm2 + frailty")
 ##
 test_that("base", {
