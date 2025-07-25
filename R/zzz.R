059b6f898ec33ebe57b3453e8a5f29e700a5ce2b
Mark Clements <mark.clements@ki.se>
2025-07-25 16:40:23 +0200

    Remove testing options in .onLoad()

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,5 @@
 .onLoad <- function(libname, pkgname) {
-    ## trace("formals", tracer = quote({
-    ##     cat(">>> formals called in R CMD check with object of class:", class(fun), "\n")
-    ##     print(sys.calls())
-    ## }), print = FALSE)
-    options(warn = 3, error = utils::recover)
+    ## options(warn = 3, error = utils::recover)
 }
 
 .onUnload <- function (libpath) {
